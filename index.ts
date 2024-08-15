84dc91d258ddddcfd3ebd7dc65818f2e25c5585f
Sandipan Mahata <Sandipan.m@muvin.in>
2024-08-15 11:16:52 +0530

    refactored

--- index.ts
+++ index.ts
@@ -1,50 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-const Redis = require("ioredis");
-const { Server } = require("socket.io");
-const http = require("http");
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+
+import config from "./config/common";
 import router from "./routes";
+import startupInitialize from "./startup";
 
-const subscriber = new Redis(
-  `rediss://default:${process.env.REDIS_PASSWORD}@caching-1529a761-sandipan-050b.g.aivencloud.com:24119`
-);
-
+/* express app initialization */
 const app = express();
 const server = http.createServer(app);
-const PORT = 9000;
 
-const io = new Server(server, {
-  cors: "*",
-});
-
-io.on("connection", (socket: any) => {
-  socket.on("subscribe", (channel: any) => {
-    socket.join(channel);
-  });
-});
-
+/* middlewares */
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", (req: any, res: any) => {
-  return res.json({
-    app: "vercel clone",
-    version: "1.0.0",
-  });
-});
-
+/* use express router */
 app.use(router);
 
-async function initRedisSubscribe() {
-  subscriber.psubscribe("logs:*");
-  subscriber.on("pmessage", (pattern: any, channel: any, message: any) => {
-    io.to(channel).emit("message", message);
-  });
-}
+/* creation of socket server */
+const io = new Server(server, {
+  cors: "*" as any,
+});
 
-initRedisSubscribe();
+/* run startup initializer */
+startupInitialize(io);
 
-server.listen(process.env.PORT || PORT, () =>
+const PORT = process.env.PORT || config.PORT;
+server.listen(PORT, () =>
   console.log(`Api server is listening on port ${PORT}`)
 );
--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,14 @@
-import express from "express";
+import express, { Response, Request } from "express";
 import projectRouter from "./project";
 
 const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  return res.json({
+    app: "vercel clone",
+    version: "1.0.0",
+  });
+});
 
 router.use("/v1/project", projectRouter);
 
